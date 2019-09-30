de3dfbd50ad50027d9bf0ef364e6b271680cdfa6
Raven Raven <raven_180691@mail.ru>
2019-09-30 22:24:02 +0300

    Rewritten in classes.

--- HW4_Task2/Program.cs
+++ HW4_Task2/Program.cs
@@ -10,53 +10,8 @@ namespace HW4_Task2
     {
         static void Main(string[] args)
         {
-            BubbleSort();
-
+            Sort.BubbleSort();
             Console.ReadLine();
-        }
-
-        static int[] Create()
-        {
-            Random numbers = new Random();
-
-            int[] array = new int[5];
-
-            for (int i = 0; i < array.Length; i++)
-            {
-                array[i] = numbers.Next(0, 100);
-            }
-
-            foreach (var item in array)
-            {
-                Console.Write($"{item} ");               
-            }
-
-            Console.WriteLine($"\n");
-
-            return array;
-        }
-
-        static void BubbleSort()
-        {
-            int[] sortArray = Create();
-
-            for (int j = sortArray.Length - 1; j > 0; j--)
-            {
-                for (int i = 0; i < j; i++)
-                {
-                    if (sortArray[i] > sortArray[i + 1])
-                    {
-                        sortArray[i] = sortArray[i + 1] + sortArray[i];
-                        sortArray[i + 1] = sortArray[i] - sortArray[i + 1];
-                        sortArray[i] = sortArray[i] - sortArray[i + 1];
-                    }
-                }
-            }           
-            
-            foreach (var item in sortArray)
-            {
-                Console.Write($"{item} ");
-            }
-        }
+        }        
     }
 }
---
+++ HW4_Task2/Sort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4_Task2
+{
+    class Sort
+    {
+        public static void BubbleSort()
+        {
+            int[] sortArray = Create.CreateArray();
+            for (int j = sortArray.Length - 1; j > 0; j--)
+            {
+                for (int i = 0; i < j; i++)
+                {
+                    if (sortArray[i] > sortArray[i + 1])
+                    {
+                        sortArray[i] = sortArray[i + 1] + sortArray[i];
+                        sortArray[i + 1] = sortArray[i] - sortArray[i + 1];
+                        sortArray[i] = sortArray[i] - sortArray[i + 1];
+                    }
+                }
+            }
+
+            foreach (var item in sortArray)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+    }
+}
